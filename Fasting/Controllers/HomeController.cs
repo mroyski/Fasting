0bbcbe3d031481b5c12b9c801213fb675eb40aab
Mike Royski <v-miroys@microsoft.com>
2020-07-31 21:41:48 -0400

    adding create timer page

--- Fasting/Controllers/HomeController.cs
+++ Fasting/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Fasting.Controllers
             return View();
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- Fasting/Data/ApplicationDbContext.cs
+++ Fasting/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Fasting.Models;
 
 namespace Fasting.Data
 {
@@ -12,5 +13,7 @@ namespace Fasting.Data
             : base(options)
         {
         }
+        public DbSet<Fasting.Models.Rhythm> Rhythm { get; set; }
+
     }
 }
