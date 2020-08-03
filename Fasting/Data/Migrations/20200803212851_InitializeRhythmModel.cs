33b22945cb00ae0fb0f38adf90072cb13eb4aea6
Mike Royski <v-miroys@microsoft.com>
2020-08-03 17:38:48 -0400

    added migration and exported script from create view to its own file so it can be reused in the edit view

---
+++ Fasting/Data/Migrations/20200803212851_InitializeRhythmModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fasting.Data.Migrations
+{
+    public partial class InitializeRhythmModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rhythm",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ratio = table.Column<int>(nullable: false),
+                    StartTime = table.Column<DateTime>(nullable: false),
+                    EndTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rhythm", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rhythm");
+        }
+    }
+}
