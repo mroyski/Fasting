9c6e4a1297df469956aee2829a69b4c80e64cfa8
mroyski <v-miroys@microsoft.com>
2020-08-08 20:02:52 -0400

    playing around with unit tests

--- Fasting.Tests/RhythmControllerTests.cs
+++ Fasting.Tests/RhythmControllerTests.cs
@@ -1,14 +1,42 @@
-using System;
+using Fasting.Controllers;
+using Fasting.Data;
+using Fasting.Models;
+using Fasting.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Fasting.Tests
 {
     public class RhythmControllerTests
     {
-        [Fact]
-        public void Test1()
-        {
+        RhythmController underTest;
+        ApplicationDbContext context;
+        IAchievedItemService achievedItemService;
 
+        public RhythmControllerTests()
+        {
+            underTest = new RhythmController(context, achievedItemService);
+        }
+
+        [Fact]
+        public void Index_Returns_ViewResult()
+        {
+            var result = underTest.Index();
+            Assert.IsType<Task<IActionResult>>(result);
+        }
+
+        [Fact]
+        public void Index_Model_Is_Expected_Model()
+        {
+            var expectedModel = new List<Rhythm>();
+            var expectedModelType = expectedModel.GetType();
+
+            var expectedType = context.Rhythm.GetType();
+            Assert.Equal(expectedModelType, expectedType);
+            
         }
     }
 }
