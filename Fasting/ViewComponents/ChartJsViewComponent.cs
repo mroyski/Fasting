d1e28c6cb6f520f638abb86c7f98c37edb10d167
mroyski <v-miroys@microsoft.com>
2020-08-11 19:21:22 -0400

    CHARTSgit add .! It may not be the prettiest code but it is working right now, querying the db for what timers have been completed!

--- Fasting/ViewComponents/ChartJsViewComponent.cs
+++ Fasting/ViewComponents/ChartJsViewComponent.cs
@@ -1,13 +1,28 @@
-﻿using Fasting.Models;
+﻿using Fasting.Data;
+using Fasting.Models;
 using Fasting.Models.Chart;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Fasting.ViewComponents
 {
     [ViewComponent(Name = "chartjs")]
     public class ChartJsViewComponent : ViewComponent
     {
+        private readonly ApplicationDbContext _context;
+        private readonly int completed;
+        private readonly int notCompleted;
+
+        public ChartJsViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+            var completionList = _context.Rhythm.ToList();
+            completed = completionList.Where(x => x.Achieved == true).Count();
+            notCompleted = completionList.Where(x => x.Achieved == false).Count();
+        }
+
         public IViewComponentResult Invoke()
         {
             // Ref: https://www.chartjs.org/docs/latest/
@@ -17,25 +32,17 @@ namespace Fasting.ViewComponents
                 responsive: true,
                 data:
                 {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    labels: ['Not Completed', 'Completed'],
                     datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
+                        label: 'Achievement',
+                        data: [" + $"{notCompleted}, {completed}" + @"],
                         backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
                             ],
                         borderColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
                             ],
                         borderWidth: 1
                     }]
